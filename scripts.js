71e4e9d3f1564505db1690f624cedf15a27afb03
shena3333 <shena3333@mail.ru>
2023-11-25 21:07:12 +0300

    add style in css and js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,12 @@
 let symbols = ['C', '←', '/', '*', '7', '8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '=', '0', '.', 'MS', 'MR','M+', 'M-', 'MRC'];
-// нужно создать переменную container и положить в нее через querySelector элемент, внутри которого будут создаваться кнопки
-const container = document.querySelector('.buttons'); //мое
-console.log (container);// мое
-
+const container = document.querySelector('.buttons'); 
+console.log (container);
 symbols.map((value) => {
-    // создать переменую el и прописать создание кнопок через createElement
-const el=document.createElement('button'); //мое
-console.log(el); //мое
-    // присвоить value свойству textContent переменой el
-    el.textContent=value;//мое
-    // добавить через метод appendChild (ВНИМАНИЕ! Именно appendChild, а не обычный append) переменул el в container
-        container.appendChild(el);//мое
+const el=document.createElement('button'); 
+console.log(el); 
+el.className = 'butStyle';
+    el.textContent=value;
+        container.appendChild(el);
     el.addEventListener('click', () => {
         switch (value) {
             case 'C': clearScreen();
@@ -41,10 +37,7 @@ console.log(el); //мое
 })
 
 
-// создать переменную display и положить в нее контейнер с соответствующим id через метод getElementById
-const display=document.getElementById('display');// мое
-console.log (display); // мое
-// создать пустую переменую memory (мы ее будем все время переназначать, поэтому подумайте она должна быть const или let)
+const display=document.getElementById('display');
 let memory
 
 function appendToDisplay(value) {
@@ -87,4 +80,13 @@ function memorySubtract() {
 
 function memoryReadClear() {
     display.value = memory;
+}
+let button = document.querySelectorAll('.butStyle');
+for (let i = 0; i <= button.length; i++) {
+    button[i].addEventListener('mouseover', function () {
+        button[i].setAttribute('class', 'butStyle1');
+    });
+    button[i].addEventListener('mouseout', function () {
+        button[i].setAttribute('class', 'butStyle');
+    })
 }
